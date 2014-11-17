7cc0019bd5700af4ff38d72506abe13a2b373ee2
Nabil Tazi <nabil_tazi@hotmail.fr>
2014-11-17 16:27:35 +0100

    Class profile créée

---
+++ daiia/src/agent/profiler/Profile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agent.profiler;
+
+/**
+ *
+ * @author Nabil
+ */
+class Profile {
+    String name;
+    
+    public Profile(String name){
+        this.name = name;
+        
+    }
+    
+}
